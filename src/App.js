db5e7be0810641ac792f1ab57d7eee605013bf3f
Ruby Ghosh <60173326+ghoshruby@users.noreply.github.com>
2021-10-31 12:23:59 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+import Boston from './Components/Boston';
+
+function App() {
+  return (
+    <div className="App">
+     
+      
+      
+      <Boston/>
+    </div>
+  );
+}
+
+export default App;
